88d61769342e4c58997471115c0e810f26577647
Unknown <Nerrazen@outlook.fr>
2017-02-15 19:22:42 +0100

    Merge remote-tracking branch 'refs/remotes/origin/master'

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style2.css" />
+        <link rel="images" href="img"/>
+        <title>Nerrazen - Accueil</title>
+<<<<<<< HEAD
+=======
+        <script type="text/javascript" src="../js/menu_deroulant.js"></script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+>>>>>>> master
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8039968599498379",
+            enable_page_level_ads: true
+            });
+        </script>
+    </head>
+    <body>
+        <header>
+            <?php include("php/menu.php"); ?>       
+        </header>
+        <div id="corps">
+            <div class="suivre">
+                <h2>Lire mes histoires</h2>
+            </div>
+            <div class="groupe">
+                <section id="ht">
+                    <?php include("php/histoires.php"); ?>
+                </section>
+                <section id="ht2">
+                    <?php include("php/biographie.php"); ?>
+                </section>
+            </div>
+            <section>
+                <?php include("php/reseaux.php"); ?>
+            </section>
+            <section id="maj">
+                <?php  include("php/actualites.php"); ?>
+            </section>
+        </div>
+        <footer>
+              <h3>Copyright Nerrazen ©2017 - Tous droits réservés</h3>  
+        </footer>  
+    </body>
+</html>
--- php/biographie.php
+++ php/biographie.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <div id="bio">
 	<div class="g1">
 		<img src="img/anonyme.jpeg" height="128px"><p>Ajouter ici une biographie possible de l'auteur comprenant, nom, prénom, age, passion, ce qu'il aime, déteste. Ajouetr descriptifs des histoires, rajouter les projets.</p><br />
@@ -12,3 +13,8 @@
         </form>
 	</div>
 </div>
+=======
+			<div id="bio">
+				<img src="img/anonyme.jpeg" height="128px"><p>Ajouter ici une biographie possible de l'auteur comprenant, nom, prénom, age, passion, ce qu'il aime, déteste. Ajouetr descriptifs des histoires, rajouter les projets.</p>
+       	    </div>
+>>>>>>> master
